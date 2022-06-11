733cad39a865f9c0e5e0c0bfd5008e191cfe09db
aneroma <aneroma@yahoo.com@git config --global user.name diana@>
2022-06-11 20:36:34 +0100

    Helpers

--- app/src/main/java/ie/wit/hotel/helpers/ImageHelpers.kt
+++ app/src/main/java/ie/wit/hotel/helpers/ImageHelpers.kt
@@ -1,4 +1,12 @@
 package ie.wit.hotel.helpers
 
-class ImageHelpers {
+import android.content.Intent
+import androidx.activity.result.ActivityResultLauncher
+import ie.wit.hotel.R
+
+fun showImagePicker(intentLauncher : ActivityResultLauncher<Intent>) {
+    var chooseFile = Intent(Intent.ACTION_OPEN_DOCUMENT)
+    chooseFile.type = "image/*"
+    chooseFile = Intent.createChooser(chooseFile, R.string.select_hotel_image.toString())
+    intentLauncher.launch(chooseFile)
 }
